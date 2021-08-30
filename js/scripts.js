e59c142e033ee12263241000aa1f08aeae7b54ce
Slap <slap@Slap-3.local>
2021-08-30 21:41:03 +0200

    Login page ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,45 @@
-$( "#target" ).click(function() {
-    alert( "Handler for .click() called." );
+$(document).ready(function () {
+  $("#submit").click(function () {
+    if (checkForm())
+      send()
+    else
+      alert('Le formulaire est invalide');
+  })
+})
+
+function checkForm() {
+  var regexEmail = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+
+  var validEmail = regexEmail.test($("#email").val());
+  var password = $("#password").val();
+  var validCgu = $("#cgu").is(":checked");
+
+  if (validEmail && validCgu && password.length > 0)
+    return true
+  else
+    return false
+}
+
+function send() {
+  $('#submit').html('Inscription...');
+
+  $.ajax({
+    url: "https://demo.soan-solutions.io/test_front/inscription",
+    type: "get", //send it through get method
+    data: { 
+      email: $("#email").val(),
+      password: $("#password").val(),
+    },
+    success: function(response) {
+      $('#submit').html('Succès !');
+      $("#submit").prop("disabled", false);
+      location.replace("pages/invoices.html");
+    },
+    error: function(xhr) {
+      $('#submit').html('Une erreur est survenue.');
+      $("#submit").prop("disabled", false);
+    }
   });
+
+  $("#submit").prop("disabled", true);
+}
